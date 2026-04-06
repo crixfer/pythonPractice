b634267526df27a39f97a0a873c9f7b669d62267
Christopher Portorreal <107440211+crixfer@users.noreply.github.com>
2026-04-06 17:56:52 +0000

    Try and exceptions

--- practice.py
+++ practice.py
@@ -1,10 +1,26 @@
 #Practicing python
-def questions():
-    name = input("What's your name? ").title().strip()
-    greet(name)
+# def questions():
+#     name = input("What's your name? ").title().strip()
+#     greet(name)
 
 
-def greet(to):
-    print("Hi,", to, "Welcome to Wednesday!")
+# def greet(to):
+#     print("Hi,", to, "Welcome to Wednesday!")
 
-questions()
+# questions()
+
+# from random import randint
+
+# randomNumber = randint(1,10)
+# print(randomNumber)
+
+import random
+
+cards = ["queen", "king", "jack", "joker"]
+
+random.shuffle(cards)
+print(cards)
+
+for card in cards:
+    print(card)
+
